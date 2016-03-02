20bd3ef9a2cc1b9ba0343d74d8cd4a49de9c2367
Jak Charlton <jakcharlton@gmail.com>
2016-03-03 08:56:14 +1100

    Adding data object to custom exceptions

--- lib/simple_spark/exceptions.rb
+++ lib/simple_spark/exceptions.rb
@@ -1,6 +1,20 @@
 module SimpleSpark
   module Exceptions
-    class Error < StandardError; end
+    # Adding an object to the exception to pass errors back
+    # begin
+    #   raise Error.new({ id: '1' }), "a message"
+    # rescue Error => e
+    #   puts e.message # => "a message"
+    #   puts e.object # => { id: '1' }
+    # end
+    class Error < StandardError
+      attr_reader :object
+
+      def initialize(object)
+        @object = object
+      end
+    end
+
     class InvalidConfiguration < Error; end
     class DeliveryException < Error; end
   end
