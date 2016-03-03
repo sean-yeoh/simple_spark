e8af65785e645e24b700e9d488afbb35f284ab53
Jak Charlton <jakcharlton@gmail.com>
2016-03-03 18:58:55 +1100

    Docs updated for inbound domains

--- lib/simple_spark/endpoints/inbound_domains.rb
+++ lib/simple_spark/endpoints/inbound_domains.rb
@@ -8,39 +8,36 @@ module SimpleSpark
       def initialize(client)
         @client = client
       end
+      # Lists the most recent version of each template in your account.
+      # @return [Array] a list of Template hash objects
+      # @note See: https://developers.sparkpost.com/api/#/reference/templates/create-and-list/list-all-templates
 
       # Lists your inbound domains
-      #
-      # [ { "domain": "inbounddomain.test.com" }, { "domain": "inbounddomain2.test.com" } ]
-      #
-      # See: https://developers.sparkpost.com/api/#/reference/inbound-domains/list-all-inbound-domains
+      # @return [Array] a list of Inbound Domain hash objects
+      # @note See: https://developers.sparkpost.com/api/#/reference/inbound-domains/list-all-inbound-domains
       def list
         @client.call(:get, 'inbound-domains')
       end
 
       # Create an inbound domain
-      # domain_name: the domain name to create
-      #
-      # See: https://developers.sparkpost.com/api/#/reference/inbound-domains/create-and-list/create-an-inbound-domain
+      # @param domain_name [String] the domain name to create
+      # @note See: https://developers.sparkpost.com/api/#/reference/inbound-domains/create-and-list/create-an-inbound-domain
       def create(domain_name)
         @client.call(:post, 'inbound-domains', { domain: domain_name })
       end
 
       # Retrieve an inbound domain
-      # domain_name: the domain name to delete
-      #
-      # Response: { domain: domain_name }
-      #
-      # See: https://developers.sparkpost.com/api/#/reference/inbound-domains/create-and-list/retrieve-an-inbound-domain
+      # @param domain_name [String] the domain name to retrieve
+      # @return [Hash] an Inbound Domain hash object
+      # @note See: https://developers.sparkpost.com/api/#/reference/inbound-domains/create-and-list/retrieve-an-inbound-domain
       def retrieve(domain_name)
         domain_name = @client.url_encode(domain_name)
         @client.call(:get, "inbound-domains/#{domain_name}")
       end
 
       # Delete an inbound domain
-      # domain_name: the domain name to delete
-      #
-      # See: https://developers.sparkpost.com/api/#/reference/inbound-domains/retrieve-and-delete/delete-an-inbound-domain
+      # @param domain_name [String] the domain name to retrieve
+      # @note See: https://developers.sparkpost.com/api/#/reference/inbound-domains/retrieve-and-delete/delete-an-inbound-domain
       def delete(domain_name)
         domain_name = @client.url_encode(domain_name)
         @client.call(:delete, "inbound-domains/#{domain_name}")
